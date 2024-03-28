ff6b1e3bc18a38cb680e19b6872f6aa6403b0573
Ahonkhai <kakashiblack8@gmail.com>
2024-03-28 22:35:33 +0100

    first lol

--- main.cpp
+++ main.cpp
@@ -1,1 +1,10 @@
 // i'm installing MSYS2 🦆🦆
+#include <iostream>
+
+int main() {
+    std::cout << "i like pizza! 🍕" << std::endl; // ok so "<< std::endl" and "<< '\n'"
+    std::cout << "it's somewhat good, lol" << '\n';
+    std::cout << "testing another line" << '\n';
+
+    return 0;
+}
