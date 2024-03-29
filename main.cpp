db43140b64f16dfa31dd6b47f5936257b47efbf7
Ahonkhai <kakashiblack8@gmail.com>
2024-03-29 13:15:22 +0100

    Declaration and assignment

--- main.cpp
+++ main.cpp
@@ -2,9 +2,25 @@
 #include <iostream>
 
 int main() {
+    // 🦆🦆🦆
     std::cout << "i like pizza! 🍕" << std::endl; // ok so "<< std::endl" and "<< '\n'"
     std::cout << "it's somewhat good, lol" << '\n';
     std::cout << "testing another line" << '\n';
+
+    
+    int x; // Declaration 💪
+    x = 5; // Assignment 📑
+    int  y = 10; // Declaration and assignment 🤯 
+
+
+    std::cout << x << '\n';
+    std::cout << y << '\n';
+    std::cout << x + y << '\n';
+
+
+
+
+
 
     return 0;
 }
