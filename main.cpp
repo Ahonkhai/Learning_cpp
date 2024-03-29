f141727b2d2cc4f6b112c1ece50894edb869e6cf
Ahonkhai <kakashiblack8@gmail.com>
2024-03-29 14:18:07 +0100

    test

--- main.cpp
+++ main.cpp
@@ -24,10 +24,27 @@ int main() {
     int days = 20.5;
 
     std::cout << age << '\n';
-    
+    std::cout << days << '\n'; 
+    /*intergers dont work with whole numbers, 😒 its just gonna output whats behind the decimal */
 
     /* Just solved my first issue on c++ lol 🦆🦆🦆🦆
     turns out you can't start a new line without ending the last one lol */
+
+    // Double (Decimal numbers) 🍎🍎🍎🍎
+    double decimal_test = 20.5;
+
+    std::cout << decimal_test << '\n';
+
+    // Char (single characters) 🦆🦆🦆
+
+    char first_name = 'David';
+    char surname = 'Ahonkhai';
+
+
+    // quick test 💀💀💀💀
+    std::cout << "Hello, my name is" << " " << first_name << " " << surname << "i am " << age << "years old" << '\n';
+    // ok char is actually single characters like "n" and stuff like that 💀💀💀
+
 
 
     return 0;
