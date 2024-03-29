7a9868cb06c253f84f305189df8156c01757ceba
Ahonkhai <kakashiblack8@gmail.com>
2024-03-29 13:59:27 +0100

    got my first and solved my first issue lol

--- main.cpp
+++ main.cpp
@@ -11,15 +11,23 @@ int main() {
     int x; // Declaration 💪
     x = 5; // Assignment 📑
     int  y = 10; // Declaration and assignment 🤯 
-
+    int sum = x + y;
 
     std::cout << x << '\n';
     std::cout << y << '\n';
     std::cout << x + y << '\n';
+    std::cout << "sum is" << " " << sum << std::endl;
 
+    // Intergers (Whole numbers) 💪💪💪
+    int age = 18;
+    int year = 2005;
+    int days = 20.5;
 
+    std::cout << age << '\n';
+    
 
-
+    /* Just solved my first issue on c++ lol 🦆🦆🦆🦆
+    turns out you can't start a new line without ending the last one lol */
 
 
     return 0;
