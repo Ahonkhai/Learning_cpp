54246b1a8cfa3dab00b1bb6d0050366524890bfb
Ahonkhai <kakashiblack8@gmail.com>
2024-04-07 11:22:39 +0100

    I'm gonna divide each class into different files now

---
+++ math.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main () {
+    
+    double w;
+    double x = 1;
+    double y = 10;
+    double z;
+
+    z = std::max(x, y); /* This functions shows the max number between variables 😈🙏*/
+    w = std::min(x, y); /* This function shows the min number between variables 😈🙏*/
+
+    std::cout << z << '\n';
+    std::cout << w << '\n';
+    
+    
+    
+    
+    
+    return 0;
+}
