83ab7308a381985e339fb7faef7b803fb8de951c
Ahonkhai <kakashiblack8@gmail.com>
2024-04-10 03:21:29 +0100

    lol why did i add js

--- test.js
+++ test.js
@@ -12,4 +12,22 @@ function sum() {
   
   console.log("lol array sum", sum());
 
+var multiple_array = [
+  [1,2,3],
+  [4,5,6],
+  [7,8,9]
+]
 
+// Lets say i wanna log 8 what do we do 
+console.log(multiple_array[2][1])
+
+// now lets add everything
+
+var array_sum = 0
+for (let a = 0; a < multiple_array.length; a++) {
+  for (let b = 0; b < multiple_array[a].length; b++) {
+    array_sum += multiple_array[a][b]
+  }
+}
+
+console.log(array_sum)
