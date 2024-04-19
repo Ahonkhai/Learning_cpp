e9b7a34ca13d60afeaa6e7889145d4564abbe4c7
Ahonkhai <kakashiblack8@gmail.com>
2024-04-19 06:53:05 +0100

    calculator

---
+++ .vscode/cal_speedrun.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <cmath>
+
+
+
+int main() {
+
+    double first_number;
+    double second_number;
+    char math_operator;
+    double result;
+
+
+    std::cout << "Input first number" << '\n';
+    std::cin >> first_number;
+
+    std::cout << "Input operator" << '\n';
+    std::cin >> math_operator;
+
+    std::cout << "Input seecond number" << '\n';
+    std::cin >> second_number;
+
+
+    switch (math_operator)
+    {
+    case '+':
+        result = first_number + second_number;
+        std::cout << "The result is: " << result << '\n';
+        break;
+    case '-':
+        result = first_number - second_number;
+        std::cout << "The result is: " << result << '\n';
+        break;
+    case '*':
+        result = first_number * second_number;
+        std::cout << "The result is: " << result << '\n';
+        break;
+    case '/':
+        if (second_number != 0 )
+        {
+            result = first_number / second_number;
+            std::cout << "The result is: " << result << '\n';
+        }   else{
+                std::cout << "Invalid Equation: not divisible by zero" << '\n';
+        }  
+        break;
+    
+    default:
+        std::cout << "Invalid operator" << '\n';
+        break;
+        
+    }
+
+
+    return 0;
+}
