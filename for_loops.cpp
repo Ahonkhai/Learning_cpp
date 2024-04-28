6435566548ec5a3b6f943fb158b79ffca948411c
Ahonkhai <kakashiblack8@gmail.com>
2024-04-28 23:11:54 +0100

    Fizz buzz lol

---
+++ for_loops.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+int main()
+{
+
+    for (double i = 0; i < 101; i++)
+    {
+        std::cout << i << '\n';
+    }
+
+    // Write a program that prints the numbers from 1 to 100. But for multiples of three,
+    // print "Fizz" instead of the number, and for the multiples of five, print "Buzz". For numbers that are multiples of both three and five, print "FizzBuzz".
+
+    for (int i = 0; i <= 100; i++)
+    {
+        if (i % 3 == 0 && i % 5 == 0)
+        {
+            std::cout << "FizzBuzz" << '\n';
+        }
+        else if (i % 3 == 0)
+        {
+            std::cout << "Fizz" << '\n';
+        }
+        else if (i % 5 == 0)
+        {
+            std::cout << "Buzz" << '\n';
+        }
+        else
+        {
+            std::cout << i << '\n';
+        }
+    }
+
+    return 0;
+}
