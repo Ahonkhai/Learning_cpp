1c4398ffe30b74c8d4cd1b47e7d63ca6f29c86ae
Ahonkhai <kakashiblack8@gmail.com>
2024-04-28 21:44:40 +0100

    lol while_loops

---
+++ while_loops.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstring>
+
+int main() {
+
+
+    std::cout << "Hello world" << '\n';
+
+    std::string name;
+
+
+    while (name.empty()) // while loop is like an if statements but it check for a conditions infinitely
+    {   
+        std::cout << "Input name: ";
+        std::getline(std::cin, name);
+    }
+    
+    std::cout << "Hello " << name << '\n';
+
+    return 0;
+}
