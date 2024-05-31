9aea63c691f33e43f8c7f8570bcb6b80076efcef
Ahonkhai <kakashiblack8@gmail.com>
2024-05-31 19:20:01 +0100

    functions again

--- functions.cpp
+++ functions.cpp
@@ -14,23 +14,26 @@ namespace nameB {
 }
 
 // Function prototype for happy_birthday 🦆🦆🦆
-void happy_birthday(string name);
+void happy_birthday(string name, int age);
 
 int main() {
     // Demonstrate accessing variables from different namespaces 🦆🦆🦆🦆
     cout << nameA::name << " and " << nameB::name << '\n'; // Output: john and mercy 🦆🦆🦆🦆🦆
 
     // Declare a variable name inside the main function 🦆🦆🦆🦆🦆🦆
-    string name = "david";
+    string name = "David";
+    int age;
 
     // Call the happy_birthday function with the local variable name 🦆🦆🦆🦆🦆🦆🦆
-    happy_birthday(name); // sent string
+    happy_birthday(name, age); // sent string
 
     return 0;
 }
 
 // Definition of the happy_birthday function 🦆🦆🦆🦆🦆🦆🦆🦆 received the string
-void happy_birthday(string name) {
+void happy_birthday(string name, int age) {
     // Print a birthday message using the provided name 🦆🦆🦆🦆🦆🦆🦆🦆🦆 
-    cout << "Happy birthday " << name << '\n';
+    cout << "Happy birthday " << name << ", How old are you now?" << '\n';
+    cin >> age;
+    cout << "Congratulations on being " << age;
 } 
